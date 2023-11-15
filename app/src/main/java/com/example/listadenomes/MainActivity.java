28887af2bd9c6efb1f721e3357f0c6d3be7b77b4
Decio Junior <juniordascar79@hotmail.com>
2023-11-15 12:43:06 -0300

    Primeira Fase do Projeto II

--- app/src/main/java/com/example/listadenomes/MainActivity.java
+++ app/src/main/java/com/example/listadenomes/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listNomes = findViewById(R.id.listNomes);
+        listNomes = findViewById(R.id.ConstraintLayout);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getApplicationContext(),
